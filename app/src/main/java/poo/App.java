c98213dccf877ad156957f1d09f39720758e4d26
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2025-02-05 15:24:00 -0300

    atualizando versão do grale e organização do código

--- app/src/main/java/poo/App.java
+++ app/src/main/java/poo/App.java
@@ -6,16 +6,21 @@ import edu.princeton.cs.algs4.DrawListener;
 import java.awt.*;
 
 /**
- * Pequeno exemplo para ilustrar a captura
- * de evento de clique do mouse
+ * Exemplo de aplicação gráfica com interação do usuário por mouse e teclado
+ * 
+ * A aplicação desenha um tabuleiro e um círculo que pode ser movido pelo usuário com o mouse
+ * O círculo pode ser selecionado ou desmarcado com um clique do mouse
+ * A aplicação também captura eventos de teclado
+ * Pressionando a tecla 's' é exibida uma mensagem no terminal
+ * 
  */
-public class Principal implements DrawListener {
+public class App implements DrawListener {
     private final int dimensao;
     private Circulo circulo;
     private Draw draw;
     private boolean teclaPressionada;
 
-    public Principal(int dimensao) {
+    public App(int dimensao) {
         // Criando um círculo na coordenada (0,0)
         circulo = new Circulo(0, 0);
         // Objeto responsável por fazer a área de desenho (canvas)
@@ -28,14 +33,33 @@ public class Principal implements DrawListener {
         // Para saber quando uma tecla não está mais pressionada
         teclaPressionada = false;
 
-        // Indicando o número de casas da área de desenho
+        // Definindo a dimensão do tabuleiro
         this.dimensao = dimensao;
+        
+        // Definindo que a escala de cada eixo será de 0 a dimensao
         draw.setXscale(0, dimensao);
         draw.setYscale(0, dimensao);
+
+        // Habilitando o uso de double buffering para deixar o desenho mais suave
         draw.enableDoubleBuffering();
 
-        // Desenhando os objetos na tela
-        desenharTela();        
+          
+    }
+
+    /**
+     * Limpa a tela, desenha o tabuleiro e os círculos
+     */
+    public void desenharTela(){
+        // Limpa a área de desenho
+        draw.clear(Color.LIGHT_GRAY);
+        // Desenha o tabuleiro
+        this.desenharTabuleiro();
+        // Desenha o círculo
+        this.desenharCirculos();
+        draw.setPenColor(Color.BLACK);
+        draw.text(4.5, .5, "Pressione a tecla 's' e veja o terminal");
+        // Exibe a área de desenho, com todos os objetos desenhados
+        draw.show();
     }
 
     /**
@@ -55,18 +79,6 @@ public class Principal implements DrawListener {
     }
 
     /**
-     * Limpa a tela, desenha o tabuleiro e os círculos
-     */
-    public void desenharTela(){
-        draw.clear(Color.LIGHT_GRAY);
-        this.desenharTabuleiro();
-        this.desenharCirculos();
-        draw.setPenColor(Color.BLACK);
-        draw.text(4.5, .5, "Pressione a tecla 's' e veja o terminal");
-        draw.show();
-    }
-
-    /**
      * Captura o evento de botão pressionado do mouse
      * @param x coordenada X do cursor do mouse quando o botão foi pressionado
      * @param y coordenada Y do cursor do mouse quando o botão foi pressionado
@@ -74,16 +86,22 @@ public class Principal implements DrawListener {
     public void mousePressed(double x, double y) {
         // Se o usuário clicou dentro da casa do tabuleiro onde está o círculo
         if ((Math.floor(x) == circulo.getX()) && (Math.floor(y) == circulo.getY())){
+            // Troca o estado do círculo (selecionado ou não)
             circulo.marcaDesmarca();
         }else{
+            // Move o círculo para a nova posição e desmarca o círculo
             circulo.setX(x);
             circulo.setY(y);
             circulo.setSelecionado(false);
         }
-        // Limpa a tela e desenha tudo novamente (tabuleiro e círculo)
+        // Limpa a área de desenho, desenha o tabuleiro e os círculos
         desenharTela();
     }
 
+    /**
+     * Captura o evento de tecla pressionada
+     * @param c caractere da tecla pressionada
+     */
     @Override
     public void keyTyped(char c) {
         if ((c == 's') && (!teclaPressionada)){
@@ -91,14 +109,21 @@ public class Principal implements DrawListener {
             teclaPressionada = true;
         }
     }
+
+    /**
+     * Captura o evento de tecla liberada
+     * @param i código da tecla liberada
+     */
     @Override
     public void keyReleased(int i) {
         teclaPressionada = false;
     }
 
-
     public static void main(String[] args) {
-        Principal p = new Principal(8);
+
+        // Inicializa a aplicação com um tabuleiro 8x8
+        App app = new App(8);
+        app.desenharTela();
     }
 
     // Métodos da interface DrawListener que não serão usados neste exemplo
